ad69099b52e9f38ad20dade372dbe4cd19a2e370
Marani Matias Ezequiel <maranimatias@gmail.com>
2019-06-25 20:52:25 -0300

    Usin live-server to e2e test

--- test/e2e/ahorcado.spec.js
+++ test/e2e/ahorcado.spec.js
@@ -1,12 +1,27 @@
 module.exports = {
   "Cargar pagina desde github": function(client) {
     client
-      .url("https://maranimatias.github.io/Ahorcado/")
+      .url("http://127.0.0.1:8080/docs/index.html")
       .waitForElementVisible("body", 2000)
       .assert.title("Ahorcado")
       .assert.visible("h1.title.is-1")
       .pause(1000)
       .assert.containsText("h1.title.is-1", "Ahorcado")
       .end();
+  },
+  "Cambiar el nombre del jugador a 'Ahorcado'": function(client) {
+    const inputName = "input[type=text].input";
+    client
+      .url("http://127.0.0.1:8080/docs/index.html")
+      .pause(500)
+      .assert.visible(inputName)
+      .clearValue(inputName)
+      .setValue(inputName, "Ahorcado")
+      .pause(500)
+      .getValue(inputName, function(result) {
+        this.assert.equal(typeof result, "object");
+        this.assert.equal(result.value, "Ahorcado");
+        this.end();
+      });
   }
 };
