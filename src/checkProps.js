97a258791e22945d4cd83d1d4fdd2156646117cd
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-04 12:25:57 -0300

    Add test

--- src/checkProps.js
+++ src/checkProps.js
@@ -8,6 +8,9 @@ function isDefined(prop) {
 function check(object, keys = "") {
   const prop = get(object, keys, null);
   return {
+    isString: () => typeof prop === "string" || `${keys} is not a string.`,
+    isBoleand: () => typeof prop === "boolean" || `${keys} is not a boolean.`,
+    isDate: () => Date.parse(prop) > 0 || `${keys} is not a Date.`,
     isDefined() {
       return isDefined(prop) || `${keys} is not a defined.`;
     },
@@ -19,16 +22,19 @@ function check(object, keys = "") {
     isRequired() {
       return (isDefined(prop) && prop !== "") || `${keys} is required.`;
     },
-
-    isString: () => typeof prop === "string" || `${keys} is not a string.`,
-    isBoleand: () => typeof prop === "boolean" || `${keys} is not a boolean.`,
-    isDate: () => Date.parse(prop) > 0 || `${keys} is not a Date.`,
-    isEmail: () =>
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(prop) || "It is not a valid email.",
-    isPassword: () =>
+    isEmail() {
+      return (
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(prop) ||
+        "It is not a valid email."
+      );
+    },
+    isPassword() {
       // "La contraseña debe tener almenas una mayúscula, numero y mas de 6 caracteres"
-      /^((?=.*\d)(?=.*[a-záéóúíñ])(?=.*[A-ZÁÉÓÚÍÑ])?(.*[@#$%!&?¡¿!])?.{6,30})$/.test(prop) ||
-      "It is not a valid password",
+      return (
+        /^((?=.*\d)(?=.*[a-záéóúíñ])(?=.*[A-ZÁÉÓÚÍÑ])?(.*[@#$%!&?¡¿!])?.{6,30})$/.test(prop) ||
+        "It is not a valid password"
+      );
+    },
     some: items => items.some(ele => prop === ele) || `Valid values: ${items}`
   };
 }
--- test/unit/checkProps.spec.js
+++ test/unit/checkProps.spec.js
@@ -16,17 +16,17 @@ describe("Check Router Props", function() {
       });
 
       it("Prop is be True is prop is defined", function() {
-        expect(check({ _id: "prop valid" }, "_id").isDefined()).is.true;
-        expect(check({ _id: 120 }, "_id").isDefined()).is.true;
-        expect(check({ _id: true }, "_id").isDefined()).is.true;
-        expect(check({ _id: false }, "_id").isDefined()).is.true;
+        expect(check({ key: "prop valid" }, "key").isDefined()).is.true;
+        expect(check({ key: 120 }, "key").isDefined()).is.true;
+        expect(check({ key: true }, "key").isDefined()).is.true;
+        expect(check({ key: false }, "key").isDefined()).is.true;
       });
 
-      it("Prop is be False is prop is undefined", function() {
-        expect(check({ _id: "prop valid" }, "id").isDefined()).string("id is not a defined.");
-        expect(check({ _id: void 0 }, "_id").isDefined()).is.not.true;
-        expect(check({ _id: undefined }, "_id").isDefined()).is.not.true;
-        expect(check({ _id: null }, "_id").isDefined()).is.not.true;
+      it("Prop is be False is prop", function() {
+        expect(check({ key: "prop valid" }, "id").isDefined()).string("id is not a defined.");
+        expect(check({ key: void 0 }, "key").isDefined()).is.not.true;
+        expect(check({ key: undefined }, "key").isDefined()).is.not.true;
+        expect(check({ key: null }, "key").isDefined()).is.not.true;
       });
     });
 
@@ -35,15 +35,15 @@ describe("Check Router Props", function() {
         assert(typeof check().isObjectId === "function");
       });
       it("Prop is be True is prop is defined", function() {
-        expect(check({ _id: "5cf2fe6842c20f2eb0c0420c" }, "_id").isObjectId()).is.true;
+        expect(check({ key: "5cf2fe6842c20f2eb0c0420c" }, "key").isObjectId()).is.true;
       });
-      it("Prop is be False is prop is undefined", function() {
-        expect(check({ _id: "5cf2fe6842x20f2eb0c0420c" }, "_id").isObjectId()).is.not.true;
-        expect(check({ _id: "prop valid" }, "id").isObjectId()).is.not.true;
-        expect(check({ _id: "" }, "_id").isObjectId()).is.not.true;
-        expect(check({ _id: void 0 }, "_id").isObjectId()).is.not.true;
-        expect(check({ _id: undefined }, "_id").isObjectId()).is.not.true;
-        expect(check({ _id: null }, "_id").isObjectId()).is.not.true;
+      it("Prop is be False is prop", function() {
+        expect(check({ key: "5cf2fe6842x20f2eb0c0420c" }, "key").isObjectId()).is.not.true;
+        expect(check({ key: "prop valid" }, "id").isObjectId()).is.not.true;
+        expect(check({ key: "" }, "key").isObjectId()).is.not.true;
+        expect(check({ key: void 0 }, "key").isObjectId()).is.not.true;
+        expect(check({ key: undefined }, "key").isObjectId()).is.not.true;
+        expect(check({ key: null }, "key").isObjectId()).is.not.true;
       });
     });
 
@@ -52,16 +52,72 @@ describe("Check Router Props", function() {
         assert(typeof check().isRequired === "function");
       });
       it("Prop is be True is prop is defined", function() {
-        expect(check({ _id: "5cf2fe6842c20f2eb0c0420c" }, "_id").isRequired()).is.true;
-        expect(check({ _id: 120 }, "_id").isRequired()).is.true;
-        expect(check({ _id: true }, "_id").isRequired()).is.true;
-        expect(check({ _id: false }, "_id").isRequired()).is.true;
+        expect(check({ key: "5cf2fe6842c20f2eb0c0420c" }, "key").isRequired()).is.true;
+        expect(check({ key: 120 }, "key").isRequired()).is.true;
+        expect(check({ key: true }, "key").isRequired()).is.true;
+        expect(check({ key: false }, "key").isRequired()).is.true;
       });
-      it("Prop is be False is prop is undefined", function() {
-        expect(check({ _id: "" }, "_id").isRequired()).is.not.true;
-        expect(check({ _id: void 0 }, "_id").isRequired()).is.not.true;
-        expect(check({ _id: undefined }, "_id").isRequired()).is.not.true;
-        expect(check({ _id: null }, "_id").isRequired()).is.not.true;
+      it("Prop is be False is prop", function() {
+        expect(check({ key: "" }, "key").isRequired()).is.not.true;
+        expect(check({ key: void 0 }, "key").isRequired()).is.not.true;
+        expect(check({ key: undefined }, "key").isRequired()).is.not.true;
+        expect(check({ key: null }, "key").isRequired()).is.not.true;
+      });
+    });
+
+    describe("isString", function() {
+      it("isString is be a function", function() {
+        assert(typeof check().isString === "function");
+      });
+      it("Prop is be True is prop is defined", function() {
+        expect(check({ key: "5cf2fe6842c20f2eb0c0420c" }, "key").isString()).is.true;
+        expect(check({ key: "" }, "key").isString()).is.true;
+      });
+      it("Prop is be False is prop", function() {
+        expect(check({ key: void 0 }, "key").isString()).is.not.true;
+        expect(check({ key: undefined }, "key").isString()).is.not.true;
+        expect(check({ key: null }, "key").isString()).is.not.true;
+        expect(check({ key: 120 }, "key").isString()).is.not.true;
+        expect(check({ key: true }, "key").isString()).is.not.true;
+        expect(check({ key: false }, "key").isString()).is.not.true;
+      });
+    });
+
+    describe("isBoleand", function() {
+      it("isBoleand is be a function", function() {
+        assert(typeof check().isBoleand === "function");
+      });
+      it("Prop is be True is prop is defined", function() {
+        expect(check({ key: true }, "key").isBoleand()).is.true;
+        expect(check({ key: false }, "key").isBoleand()).is.true;
+      });
+      it("Prop is be False is prop", function() {
+        expect(check({ key: "5cf2fe6842c20f2eb0c0420c" }, "key").isBoleand()).is.not.true;
+        expect(check({ key: "" }, "key").isBoleand()).is.not.true;
+        expect(check({ key: void 0 }, "key").isBoleand()).is.not.true;
+        expect(check({ key: undefined }, "key").isBoleand()).is.not.true;
+        expect(check({ key: null }, "key").isBoleand()).is.not.true;
+        expect(check({ key: 120 }, "key").isBoleand()).is.not.true;
+      });
+    });
+
+    describe("isDate", function() {
+      it("isDate is be a function", function() {
+        assert(typeof check().isDate === "function");
+      });
+      it("Prop is be True is prop is defined", function() {
+        expect(check({ key: new Date() }, "key").isDate()).is.true;
+        expect(check({ key: new Date().toUTCString() }, "key").isDate()).is.true;
+        expect(check({ key: new Date().toISOString() }, "key").isDate()).is.true;
+      });
+      it("Prop is be False is prop", function() {
+        expect(check({ key: "5cf2fe6842c20f2eb0c0420c" }, "key").isDate()).is.not.true;
+        expect(check({ key: "" }, "key").isDate()).is.not.true;
+        expect(check({ key: void 0 }, "key").isDate()).is.not.true;
+        expect(check({ key: undefined }, "key").isDate()).is.not.true;
+        expect(check({ key: null }, "key").isDate()).is.not.true;
+        expect(check({ key: 120 }, "key").isDate()).is.not.true;
+        expect(check({ key: false }, "key").isDate()).is.not.true;
       });
     });
   });
